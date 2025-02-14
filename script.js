be40b37de16877c8159779d061b0179691746a88
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-14 08:55:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const colorSelect = document.getElementById("colorSelect");
+    const removeButton = document.querySelector("input[type='button']");
+
+    removeButton.addEventListener("click", function () {
+        const selectedIndex = colorSelect.selectedIndex;
+        if (selectedIndex !== -1) {
+            colorSelect.remove(selectedIndex);
+        }
+    });
+});
